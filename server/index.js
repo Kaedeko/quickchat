ceaac13f35e1ed968de9efd7fde6abf244260eee
Kaedeko <jade.k.beesley@gmail.com>
2019-05-22 14:32:48 +0100

    Now broadcasts to all connected users.

--- server/index.js
+++ server/index.js
@@ -2,11 +2,19 @@ import ws from "ws";
 
 const wss = new ws.Server({port: 54877});
 
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === ws.OPEN) {
+            client.send(data);
+        }
+    });
+};
+
 wss.on('connection', ws => {
     ws.on('message', data => {
         const msg = JSON.parse(data);
         console.log(`Got message from => ${msg.nickname} => ${msg.message}`);
-        ws.send(data);
+        wss.broadcast(data);
     });
     ws.send(JSON.stringify({message: "Connected!", encrypted: false, nickname: "Server"}));
 })
